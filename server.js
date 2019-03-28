3c46ec782c9d9672cddfec1cee79169b14dc6252
Matthew James Bialostocki <matt.bialostocki@gmail.com>
2019-03-29 11:04:19 +1300

    server set

--- server.js
+++ server.js
@@ -1,9 +1,38 @@
+// Dependincies
 const express = require('express')
-const server = express() 
 const hbs = require('express-handlebars')
+const data = require('./data.json')
 
+const server = express()
+
+// Export
 module.exports = server
 
+// Middleware
+server.engine('hbs', hbs({
+  defaultLayout: 'main',
+  extname: 'hbs'
+}))
+server.set('view engine', 'hbs')
+
+server.use(express.static('public'))
+server.use(express.urlencoded({ extended: false }))
+
+// Routes
+// Home page
 server.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>')
+  res.redirect('/drink')
 })
+server.get('/drink', (req, res) => {
+  res.send('hello World')
+})
+// Page displaying drink after recieving POST
+server.get('/drink/:id', (req, res) => {
+  // res.render()
+})
+// server.post('/drink', (req, res) => {
+//   const ingredient = req.body.ingredient
+//   const newArr = data.ingredients.filter(i => {
+//     if()
+//   })
+// })
