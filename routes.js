db719392d75ced754cdb319366426eb2f43dbd08
John Winiata Tapsell <johntapsell@windowslive.com>
2019-03-29 18:14:39 +1300

    refactor code

--- routes.js
+++ routes.js
@@ -5,38 +5,20 @@ const data = require('./data.json')
 module.exports = router
 
 router.get('/', (req, res) => {
-  res.redirect('/drink')
-})
-
-router.get('/drink', (req, res) => {
-  res.render('partials/form', data)
+  res.render('home')
 })
 
 // Page displaying drink after recieving POST
 router.get('/drink/:id', (req, res) => {
-  const getId = data.drinks.find(i => i.id === (req.params.id))
-  res.render('partials/result', getId)
+  const drink = data.drinks.find(i => i.id === (req.params.id))
+  res.render('result', drink)
 })
 
 router.post('/drink', (req, res) => {
   const ingredient = req.body.ingredient
-  let randomId
-  const newArr = []
-  data.drinks.filter(current => {
-    current.ingredients.find(string => {
-      if (string === ingredient) {
-        newArr.push([current.id])
-        randomId = newArr[Math.floor(Math.random() * newArr.length)]
-        console.log(randomId)
-      }
-    })
+  const newArr = data.drinks.filter(drink => {
+    return drink.ingredients.includes(ingredient)
   })
-  // const id = Math.random(newArr)
-  res.redirect(`drink/${randomId}`)
+  const random = newArr[Math.floor(Math.random() * newArr.length)]
+  res.redirect(`/drink/${random.id}`)
 })
-// router.get('/drink/add', (req, res) => {
-//   res.render('add-form', data)
-// }
-// router.post('/drink/add', (req, res) => {
-
-// }
